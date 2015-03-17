a8b992d27ecf5a12ce2a7be90e3e6fc7d6b9fa8a
jat023 <jat023@mail.usask.ca>
2015-03-17 17:39:21 +0000

    Updated some color specifications and code for generating the graph

--- moduleTwo/basics2.py
+++ moduleTwo/basics2.py
@@ -1,13 +1,17 @@
 import imgproc
 from imgproc import *
+from time import sleep
 import matplotlib.pyplot as plt
 import networkx as nx
+import os
+import math
+import RPi.GPIO as GPIO
 
 #open webcame to take picture
 ##camera = Camera(160, 120)
 
 #open a viewer window to display images
-#viewer = Viewer(819, 460, "The first step")
+viewer = Viewer(819, 460, "The first step")
 
 #take picture from camera
 ##img = camera.grabImage()
@@ -16,91 +20,61 @@ import networkx as nx
 img = Image("/home/pi/Desktop/381map.bmp")
 
 #display image in viewer
-#viewer.displayImage(img)
+viewer.displayImage(img)
 
+#constants used in the algorithm
+ref_red = 192
+ref_blue = 80
+ref_green = 80
+threshold = 96 
+m = img.width	#x axis
+n = img.height	#y axis
 
+''' This creats our graph; remove comments when needed
+G = nx.grid_2d_graph(m, n, periodic=False, create_using=None)
+print(G.number_of_nodes())
+print(G.number_of_edges())
+print("Done creating graph")
+print("")
+print("Processing image...")
 '''
+
 #iterate over each pixel in image
 for x in range (0, img.width):
 	for y in range(0, img.height):
 		#Get value at xth column and yth row, place intensities into variables		
 		red, green, blue = img[x,y]
-'''
 
+		if (red == blue and blue == green):
+			#RGB values are the same; they will be a shade of grey, wht, or blk
+			#remove edges from this node
+		
+		
 
+		'''
+		# subtract the pixel colour from the reference
+		d_red = ref_red - red
+		d_green = ref_green - green
+		d_blue = ref_blue - blue
 
-G = nx.Graph()
+		# length of the difference vector
+		length = math.sqrt( (d_red * d_red) + (d_green * d_green) + (d_blue * d_blue) )
 
-for y in range(0, img.height):
-	for x in range(0, img.width):
-		G.add_node((x,y))
-
-print(G.number_of_nodes())
-
-for y in range(1, (img.height - 1)):
-	for x in range(1, (img.width - 1)):
-		G.add_edge((x,y),(x-1,y))	#left
-		G.add_edge((x,y),(x-1,y-1))	#above left
-		G.add_edge((x,y),(x, y-1))	#above
-		#G.add_edge((x,y),(x+1,y-1))	#above right
-
-print(G.number_of_edges())
-
-
-
-'''
-------------create graph of the parsed image------------
-
-Create/set graph variable
-Get color value of pixel
-
-For each pixel on x axis
-	For each pixel on y axis
-
-		if x is still in the top row
-
-			if x is the first pixel
-				add x to graph
-			else
-				add x to graph
-
-				check pixel(x) color value against pixel(x-1) color value	(LEFT)
-				if able
-					add edge to graph to connect pixel(x) and pixel(x-1)
-					set edge value based on color of the pixels
-
-		else
-			add (x,y) to graph
-
-			check (x,y) color value against (x-1,y) color value		(LEFT)
-			if able 
-				add edge to graph to connect (x,y) and (x-1,y)
-				set edge value based on color of the pixels
-
-			check (x,y) color value against (x, y-1) color value		(ABOVE)
-			if able 
-				add edge to graph to connect (x,y) and (x, y-1)
-				set edge value based on color of the pixels
-
-			check (x,y) color value against (x-1, y-1) color value		(ABOVE LEFT)
-			if able 
-				add edge to graph to connect (x,y) and (x-1, y-1)
-				set edge value based on color of the pixels
-
-			if x is NOT the last pixel on the image
-				check (x,y) color value against (x+1, y-1) color value		(ABOVE RIGHT)
-				if able 
-					add edge to graph to connect (x,y) and (x+1, y-1)
-					set edge value based on color of the pixels
-------------------------------------------------------------
-'''
+		if length > threshold:
+			print(length)
+			print(threshold)
+			img[x, y] = 0, 0, 0
+		else:
+			img[x, y] = 255, 255, 255
+		'''
 
 
 
 #display the image again
-#viewer.displayImage(img)
+viewer.displayImage(img)
 
 #delay before closing window (ms)
-waitTime(5000)
+sleep(5)
+#waitTime(5000)
 
 #end of script
