77ca949ea438b2cd997b46d6d2d548564f0f9d6a
jat023 <jat023@mail.usask.ca>
2015-03-18 17:07:17 -0700

    added in BitArray

--- moduleTwo/write.py
+++ moduleTwo/write.py
@@ -1,6 +1,7 @@
 #Adam's write to DE2 program
 # Import the required module. 
-import RPi.GPIO as GPIO 
+import RPi.GPIO as GPIO
+from bitstring import BitArray, BitStream 
 # Set the mode to use physical numbering the pins. 
 
 GPIO.setmode(GPIO.BOARD) 
@@ -9,8 +10,7 @@ GPIO.setup(3, GPIO.OUT)
 
 p = GPIO.PWM(3, 50)    # create an object p for PWM on port 25 at 50 Hertz  
 p.start(50)             # start the PWM on 50 percent duty cycle              
-
-                         
+                        
 # pin 7 is the fake clock. 
 GPIO.setup(7,GPIO.OUT)
 
@@ -43,13 +43,39 @@ GPIO.output(37, False)
 
 GPIO.output(5, True)
 
+data = BitArray('0b10101010')
+adress = BitArray('0b10101010')
+
+def toDatBus( dat ):
+	#pin37 is most significant bit
+	GPIO.output(37, dat[0]) 
+	GPIO.output(35, dat[1]) 
+	GPIO.output(33, dat[2]) 
+	GPIO.output(31, dat[3])
+	GPIO.output(29, dat[4]) 
+	GPIO.output(23, dat[5]) 
+	GPIO.output(21, dat[6]) 
+	GPIO.output(19, dat[7])
+	print "set DatBus to:", dat.bin
+	return
+
+def toAdBus( ad ):
+	#pin8 is most significant bit
+	GPIO.output(37, ad[0]) 
+	GPIO.output(35, ad[1]) 
+	GPIO.output(33, ad[2]) 
+	GPIO.output(31, ad[3])
+	GPIO.output(29, ad[4]) 
+	GPIO.output(23, ad[5]) 
+	GPIO.output(21, ad[6]) 
+	GPIO.output(19, ad[7])
+	print "set AdBus to:", dat.bin
+	return
+
 try:
     # here you put your main loop or block of code
-    p.start(50)             # start the PWM on 50 percent duty cycle  
-                        # duty cycle value can be 0.0 to 100.0%, floats are OK  
-                        #p.ChangeDutyCycle("number between 1-100")    
-  
-    #p.ChangeFrequency(100)  # change the frequency to 100 Hz (floats also work)  
+    p.start(50) 
+    #p.ChangeFrequency(100)  # change the frequency to # Hz  
     
     while counter < 3000000:
     
